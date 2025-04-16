2cbeb46fb5476f5ec812a357c82c62e8ce51b452
Art1cus <61352379+Art1cus@users.noreply.github.com>
2025-04-16 22:07:59 +0200

    2024 petanque editions now the changes happen that will make it work for the tournament in 2025

--- frontend/src/components/login.rs
+++ frontend/src/components/login.rs
@@ -14,10 +14,8 @@ struct LoginRequest {
 pub fn login_view() -> Html {
     let password = use_state(|| "".to_string());
     let message = use_state(|| "".to_string());
-    let check_message = use_state(|| "".to_string());
     let password_clone = password.clone();
     let message_clone = message.clone();
-    let check_message_clone = check_message.clone();
     let password_on_input = Callback::from(move |e: InputEvent| {
         let input: web_sys::HtmlInputElement = e.target_unchecked_into();
         password_clone.set(input.value());
@@ -46,27 +44,18 @@ pub fn login_view() -> Html {
         });
     });
 
-    let on_auth_check = Callback::from(move |_| {
-        let check_message = check_message.clone();
-
-        wasm_bindgen_futures::spawn_local(async move {
-            let resp =request_get::<String>(format!("/check_auth")).await;
-            match resp {
-                Ok(_) => check_message.set("Authenticated".to_string()),
-                Err(Error::Unauthorized) => check_message.set("Not authenticated".to_string()),
-                Err(_) => check_message.set("Something went wrong".to_string()),
-            }
-        });
-    });
-
     html! {
-        <div style="text-align: center; margin-top: 5rem;">
-            <h2>{ "Please log in" }</h2>
-            <input type="password" placeholder="Password" oninput={password_on_input} />
-            <button onclick={on_login}>{ "Login" }</button>
-            <p>{ (*message_clone).clone()}</p>
-            <button onclick={on_auth_check}>{ "auth_check" }</button>
-            <p>{ (*check_message_clone).clone() }</p>
+    <div class="home-page">
+        <div class="container page">
+            <div class="card round">
+                <div class="card-body">
+                    <h2>{ "Please log in" }</h2>
+                    <input style="margin-bottom:10px;" type="password" placeholder="Password" oninput={password_on_input} />
+                    <button onclick={on_login} class="btn btn-lg btn-primary">{ "Login" }</button>
+                    <p>{ (*message_clone).clone()}</p>
+                </div>
+            </div>
         </div>
+    </div>
     }
 }
